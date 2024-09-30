34a2dad196b8e53c4584f4b0254dbbf4585f00df
Felipe Fonseca <felipeaf196@gmail.com>
2024-09-30 15:04:04 -0300

    Adicionado router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const router = require('./router')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(router)
 
 app.listen(3000, () => {
     console.log('server running on port 3000')
