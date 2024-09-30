8b3c1b73a02215c26975f7e1f050851406c4e35f
Felipe Fonseca <felipeaf196@gmail.com>
2024-09-30 16:14:35 -0300

    Adicionado modulo helpers + Api Errors com erros basicos da aplicacao (bad request, unauthorized, notfound)

--- src/router.js
+++ src/router.js
@@ -1,15 +1,8 @@
 const express = require('express')
-const { findAllUsers } = require('./repositories/userRepository')
-const { findAllProducts } = require('./repositories/productRepository')
-const { findAllOrders } = require('./repositories/orderRepository')
 const router = express.Router()
 
-
 router.get('/', async (req, res) => {
-
-    throw new Error()
     res.json('ok')
-
 })
 
 module.exports = router
