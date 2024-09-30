184373abc082ccc90b8cd5e612adcb261e09a0f2
Felipe Fonseca <felipeaf196@gmail.com>
2024-09-30 15:45:59 -0300

    Fix: nome do order repository no singular

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { findAllUsers } = require('./repositories/userRepository')
 const { findAllProducts } = require('./repositories/productRepository')
-const { findAllOrders } = require('./repositories/ordersRepository')
+const { findAllOrders } = require('./repositories/orderRepository')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
