4cd46748a33323aa5e0ab025925888586b468944
Felipe Fonseca <felipeaf196@gmail.com>
2024-10-03 19:49:39 -0300

    Adicionada validação de cpf no schema pela lib cpf-cnpj-validator

--- src/schemas/createUserSchema.js
+++ src/schemas/createUserSchema.js
@@ -1,4 +1,5 @@
 const yup = require('yup')
+const { cpf } = require('cpf-cnpj-validator');
 
 let newUserSchema = yup.object({
     nome: yup.string().min(3, "Nome minimo 3 letras").max(50).required("Nome obrigatório"),            // nome
@@ -9,7 +10,8 @@ let newUserSchema = yup.object({
         .required('A data de nascimento é obrigatória')
         .min(new Date(1900, 0, 1), 'A data deve ser posterior a 01/01/1900')
         .max(new Date(), 'A data não pode ser no futuro'),
-    cpf: yup.string().min(11).max(11).required("Cpf obrigatório"),             // cpf
+    cpf: yup.string().min(11).max(11).required("Cpf obrigatório"),                      // cpf
+    // .test(('cpf-valido', 'CPF inválido', value => cpf.isValid(value))),             // cpf validation
     telefone: yup.string().required(),                                         // telefone
 });
 
