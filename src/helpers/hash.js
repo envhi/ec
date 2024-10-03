ec3b2c389285f04f19758a69adbdb32a7500bfe9
Felipe Fonseca <felipeaf196@gmail.com>
2024-10-03 19:41:04 -0300

    Alterado nome do parametro no helper hash

--- src/helpers/hash.js
+++ src/helpers/hash.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcrypt')
 
-const hashPassword = async (plainPassword) => {
+const hashPassword = async (plainText) => {
     const saltRounds = 10;
-    return await bcrypt.hash(plainPassword, saltRounds);
+    return await bcrypt.hash(plainText, saltRounds);
 }
 
 module.exports = hashPassword
